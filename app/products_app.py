ac0697d6037a7e4658a9f3e90b483d3072ec8855
Mike Miles <mm8932@stern.nyu.edu>
2017-07-19 20:09:17 -0400

    Checkpoint II - Reading to CSV File

--- app/products_app.py
+++ app/products_app.py
@@ -1,28 +1,53 @@
+import csv
+
+
+# READ PRODUCTS CSV
+
+products = []
+
+csv_file_path = "/Users/michaelmiles/Desktop/crud-application/data/products.csv"
+
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        products.append(row)
 
 menu = """
     Hi.
 
     Welcome to the products app.
 
-    There are 100 products.
+    There are {0} products.
 
     Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
 
     Please choose an operation:
 
-"""
+""".format(len(products))
 
-chosen_operation = input(menu)
+print(menu)
 
-if chosen_operation.title() == "List":
-    print("LISTING PRODUCTS")
-elif chosen_operation.title() == "Show":
-    print("SHOWING A PRODUCT")
-elif chosen_operation.title() == "Create":
-    print("CREATING A PRODUCT")
-elif chosen_operation.title() == "Update":
-    print("UPDATING A PRODUCT")
-elif chosen_operation.title() == "Destroy":
-    print("DESTROYING A PRODUCT")
-else:
-    print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+#chosen_operation = input(menu)
+#chosen_operation = chosen_operation.title()
+#
+#def list_products():
+#    print("LISTING PRODUCTS")
+#
+#def show_product():
+#    print("SHOWING A PRODUCT")
+#
+#def create_product():
+#    print("CREATING A PRODUCT")
+#
+#def update_product():
+#    print("UPDATING A PRODUCT")
+#
+#def destroy_product():
+#    print("DESTROYING A PRODUCT")
+#
+#if chosen_operation == "List": list_products()
+#elif chosen_operation == "Show": show_product()
+#elif chosen_operation == "Create": create_product()
+#elif chosen_operation == "Update": update_product()
+#elif chosen_operation == "Destroy": destroy_product()
+#else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
